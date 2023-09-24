58030152d78b15cbb48bee8e00152d7a85e7ab51
devmashum <dev.mashum@gmail.com>
2023-09-24 12:03:59 +0200

    first commit

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,12 @@
+import Home from "../Pages/Home/Home";
+
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Home></Home>
+        </div>
+    );
+};
+
+export default MainLayout;
